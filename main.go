f1f86bef8683852a7e221732757456c42a8078f3
Andrew Lee <andrewslee030401@gmail.com>
2024-02-05 02:06:46 -0500

    added feeds migrations and queries

--- main.go
+++ main.go
@@ -55,8 +55,11 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
-	v1Router.Get("/users", apiCfg.handlerGetUser)
-
+	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	
+	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
+	
 	// /v1/healthz
 	router.Mount("/v1", v1Router)
 
